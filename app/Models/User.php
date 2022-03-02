d05a16d3e6e0e7c6072d092de016d645c806ab65
rzabhd <rzxsism80@gmail.com>
2022-03-02 13:41:29 -0500

    user class modified

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,9 +19,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'lastname',
         'email',
         'password',
     ];
+
+
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for serialization.
