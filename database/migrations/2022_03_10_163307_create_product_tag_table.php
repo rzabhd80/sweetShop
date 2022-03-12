909a8ca528866c60532dcfb4594c110f82657b91
rzabhd <rzxsism80@gmail.com>
2022-03-12 10:05:41 -0500

    product tag table modified

--- database/migrations/2022_03_10_163307_create_product_tag_table.php
+++ database/migrations/2022_03_10_163307_create_product_tag_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->unsignedBigInteger("product_id");
             $table->unsignedBigInteger("tag_id");
             $table->foreign("product_id")->references("id")->on("products")->onDelete("cascade");
-            $table->foreign("tag_id")->references("id")->on("tag")->onDelete("cascade");
+            $table->foreign("tag_id")->references("id")->on("tags")->onDelete("cascade");
             $table->timestamps();
         });
     }
