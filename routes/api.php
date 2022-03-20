480a4ce420889ce6f2650b9ea21d4bfbdd6d757f
rzabhd <rzxsism80@gmail.com>
2022-03-20 10:28:13 -0400

    userAdd method route added

--- routes/api.php
+++ routes/api.php
@@ -35,4 +35,5 @@ Route::prefix("/admin")->middleware("adminRole")->group(function () {
     Route::post("/new_product", [\App\Http\Controllers\AdminController::class, "addProduct"]);
     Route::put("/edit_product/{id}", [\App\Http\Controllers\AdminController::class, "edit_product"]);
     Route::delete("/delete_product/{id}", [\App\Http\Controllers\AdminController::class, "delete_product"]);
+    Route::post("/add_user", [\App\Http\Controllers\AdminController::class, "addUser"]);
 });
