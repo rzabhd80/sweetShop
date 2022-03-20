ba4f8ef85d4a28f0845539bbbeeed5351116d0e9
rzabhd <rzxsism80@gmail.com>
2022-03-20 09:08:10 -0400

    admin controller test passed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
         ], 201);
     }
 
-    public function edit_product($id)
+    public function edit_product($id): \Illuminate\Http\JsonResponse
     {
         $check = \request()->validate([
             "new_name" => "required|min:3",
@@ -40,5 +40,17 @@ class AdminController extends Controller
         $product->available = $check["available"];
         $product->available_number = $check["available_number"];
         $product->save();
+        return response()->json(["product edited"], 200);
+    }
+
+    public function delete_product($id): \Illuminate\Http\JsonResponse
+    {
+        $product = Product::find($id);
+        if ($product == null)
+            return response()->json(["product not found"], 404);
+        else {
+            $product->delete();
+            return response()->json(['product deleted'], 200);
+        }
     }
 }
